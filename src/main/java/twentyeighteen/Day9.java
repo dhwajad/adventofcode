3f8f922d1213364aad49f565f7bc8c06ab284f18
dhwajad <dhwajad@gmail.com>
2018-12-10 18:00:57 -0500

    Day9 part2 complete, updated data structure

--- src/main/java/twentyeighteen/Day9.java
+++ src/main/java/twentyeighteen/Day9.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.LocalTime;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class Day9 {
 
@@ -19,51 +21,50 @@ public class Day9 {
         }
     }
 
-    public static void main(String[] args) {
-        int numPlayers = Integer.parseInt(string.substring(0, string.indexOf("players")).trim());
-        System.out.println("numPlayers = " + numPlayers);
-        int totalMarbles = Integer.parseInt(string.substring(string.indexOf("worth") + 5, string.indexOf("points")).trim());
-        System.out.println("totalMarbles = " + totalMarbles);
+    private static int numPlayers = Integer.parseInt(string.substring(0, string.indexOf("players")).trim());
+    private static int totalMarbles = Integer.parseInt(string.substring(string.indexOf("worth") + 5, string.indexOf("points")).trim());
 
+    public static void main(String[] args) {
+
+        ArrayDeque<Integer> circle = new ArrayDeque<>(totalMarbles);
         Map<Integer, Long> playerScore = new HashMap<>();
-        List<Integer> circle = new ArrayList<>(totalMarbles);
-        int currentIndex = 0;
         for (int i = 0; i <= totalMarbles; i++) {
-            if(i % 100000 == 0) {
-                System.out.println("i = " + i);
-                System.out.println(LocalTime.now());
-            }
             int playerNum = i % numPlayers == 0 ? numPlayers : i % numPlayers;
             if(i != 0 && i % 23 == 0) {
+                //remove on multiple of 23
                 int score = i;
-                currentIndex = currentIndex - 7;
-                while (currentIndex < 0) {
-                    currentIndex = currentIndex >= 0 ? currentIndex : currentIndex + circle.size();
-                }
-                score += circle.remove(currentIndex);
+                rotate(circle, -7);
+                score += circle.removeLast();
                 if(playerScore.containsKey(playerNum)) {
                     playerScore.put(playerNum, playerScore.get(playerNum) + score);
                 } else {
                     playerScore.put(playerNum, Integer.toUnsignedLong(score));
                 }
-                currentIndex = currentIndex + 2;
-                while (currentIndex > circle.size()) {
-                    currentIndex = currentIndex <= circle.size() ? currentIndex : currentIndex - circle.size();
-                }
-
             } else {
-                circle.add(currentIndex, i);
-                if((i + 1)  % 23 != 0) {
-                    currentIndex = currentIndex + 2;
-                    while (currentIndex > circle.size()) {
-                        currentIndex = currentIndex <= circle.size() ? currentIndex : currentIndex - circle.size();
-                    }
-                }
+                //add if not multiple of 23
+                circle.addLast(i);
+            }
+            if((i + 1)  % 23 != 0) {
+                rotate(circle, 2);
             }
         }
         Map.Entry<Integer, Long> integerIntegerEntry = playerScore.entrySet().stream()
                 .max(Map.Entry.comparingByValue())
-                .get();
+                .orElse(null);
         System.out.println("value = " + integerIntegerEntry);
+    }
+
+    private static void rotate(ArrayDeque<Integer> circle, int byIndex) {
+        if(byIndex > 0) {
+            //clockwise
+            for (int i = 0; i < byIndex; i++) {
+                circle.addFirst(circle.removeLast());
+            }
+        } else if (byIndex < 0) {
+            //anticlockwise
+            for (int i = 0; i > byIndex; i--) {
+                circle.addLast(circle.removeFirst());
+            }
+        }
     }
 }
